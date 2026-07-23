4daf46a6341b031fd282ae22f59f6cba5ba9d82b
snasy6 <150383333+snasy6@users.noreply.github.com>
2026-07-23 14:57:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,97 @@
-function postMessage(){
+// Firebase imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { 
+    getDatabase, 
+    ref, 
+    push, 
+    onValue 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+
+// Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-3DzWj-EY8ycWAtibBDqNkzojalYRjbI",
+  authDomain: "bances-pirate-emporium.firebaseapp.com",
+  databaseURL: "https://bances-pirate-emporium-default-rtdb.firebaseio.com",
+  projectId: "bances-pirate-emporium",
+  storageBucket: "bances-pirate-emporium.firebasestorage.app",
+  messagingSenderId: "193085375114",
+  appId: "1:193085375114:web:4380e8157dc1d93d96a373"
+};
+
+
+// Start Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+
+// Post a message
+
+window.postMessage = function(){
 
     let name = document.getElementById("name").value;
     let message = document.getElementById("message").value;
+
 
     if(name === "" || message === ""){
         alert("Enter a name and message!");
         return;
     }
 
-    let post = document.createElement("div");
 
-    post.className = "post";
+    push(ref(database, "messages"), {
 
-    post.innerHTML =
-    "<b>🏴‍☠️ " + name + "</b><br>" +
-    message;
+        name: name,
+        message: message,
+        time: new Date().toLocaleString()
 
-    document.getElementById("messages")
-    .prepend(post);
+    });
+
 
     document.getElementById("message").value = "";
 
-}
+};
+
+
+// Load messages
+
+const messagesRef = ref(database, "messages");
+
+
+onValue(messagesRef, (snapshot)=>{
+
+
+    const box = document.getElementById("messages");
+
+    box.innerHTML = "";
+
+
+    snapshot.forEach((child)=>{
+
+
+        let data = child.val();
+
+
+        box.innerHTML += `
+
+        <div class="post">
+
+            <b>🏴‍☠️ ${data.name}</b>
+            <br>
+
+            <small>${data.time}</small>
+
+            <p>${data.message}</p>
+
+        </div>
+
+        `;
+
+
+    });
+
+
+});
