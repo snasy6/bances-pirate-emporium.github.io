254b60a855516b8e3cc29c59b6b5cfea33b4f735
snasy6 <150383333+snasy6@users.noreply.github.com>
2026-07-23 15:28:54 -0500

    Refactor authentication logic and improve user signup

--- auth.js
+++ auth.js
@@ -1,10 +1,27 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+getAuth,
+createUserWithEmailAndPassword
+
+}
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+import {
+
+getDatabase,
+ref,
+set
+
+}
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
 
 
 const firebaseConfig = {
@@ -13,91 +30,100 @@ apiKey: "AIzaSyC-3DzWj-EY8ycWAtibBDqNkzojalYRjbI",
 
 authDomain: "bances-pirate-emporium.firebaseapp.com",
 
-databaseURL: "https://bances-pirate-emporium-default-rtdb.firebaseio.com",
+databaseURL:"https://bances-pirate-emporium-default-rtdb.firebaseio.com",
 
-projectId: "bances-pirate-emporium",
+projectId:"bances-pirate-emporium",
 
-storageBucket: "bances-pirate-emporium.firebasestorage.app",
+storageBucket:"bances-pirate-emporium.firebasestorage.app",
 
-messagingSenderId: "193085375114",
+messagingSenderId:"193085375114",
 
-appId: "1:193085375114:web:4380e8157dc1d93d96a373"
+appId:"1:193085375114:web:4380e8157dc1d93d96a373"
 
 };
 
 
+
 const app = initializeApp(firebaseConfig);
+
 
 const auth = getAuth(app);
 
 
-// CREATE ACCOUNT
-
-const signup = document.getElementById("signupButton");
-
-if(signup){
-
-signup.onclick = () => {
-
-let email = document.getElementById("email").value;
-
-let password = document.getElementById("password").value;
+const database = getDatabase(app);
 
 
-createUserWithEmailAndPassword(auth,email,password)
+
+document.getElementById("signupButton").onclick = () => {
+
+
+const username =
+document.getElementById("username").value;
+
+
+const email =
+document.getElementById("email").value;
+
+
+const password =
+document.getElementById("password").value;
+
+
+
+createUserWithEmailAndPassword(
+auth,
+email,
+password
+
+)
+
+
+.then((userCredential)=>{
+
+
+const user = userCredential.user;
+
+
+
+return set(
+
+ref(database,"users/" + user.uid),
+
+{
+
+username: username,
+
+email: email,
+
+role:"user"
+
+}
+
+);
+
+
+
+})
+
 
 .then(()=>{
+
 
 alert("Welcome aboard!");
 
 window.location.href="index.html";
 
-})
-
-.catch(error=>{
-
-alert(error.message);
-
-});
-
-};
-
-}
-
-
-// LOGIN
-
-const login = document.getElementById("loginButton");
-
-
-if(login){
-
-login.onclick = () => {
-
-
-let email = document.getElementById("email").value;
-
-let password = document.getElementById("password").value;
-
-
-signInWithEmailAndPassword(auth,email,password)
-
-.then(()=>{
-
-alert("Welcome back!");
-
-window.location.href="index.html";
 
 })
 
 
-.catch(error=>{
+.catch((error)=>{
+
 
 alert(error.message);
+
 
 });
 
 
 };
-
-}
