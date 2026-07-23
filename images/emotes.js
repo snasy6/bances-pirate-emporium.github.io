59931a61f69efdafcfc67f1628641a905d69045a
snasy6 <150383333+snasy6@users.noreply.github.com>
2026-07-23 16:24:55 -0500

    Add emotes for various themes
    Added emotes for ships, pirates, funny expressions, fire, and skeletons.

--- images/emotes.js
+++ images/emotes.js
@@ -1,1 +1,30 @@
+// 🏴‍☠️ Bances Pirate Emporium Emotes
+// Add your own below!
+// Format:
+// "shortcut": "image file"
 
+
+const emotes = {
+
+
+    // Ships
+    ":ship:" : "ship.gif",
+
+
+    // Pirates
+    ":pirate:" : "pirate.gif",
+
+
+    // Funny
+    ":lol:" : "lol.gif",
+
+
+    // Fire
+    ":fire:" : "fire.gif",
+
+
+    // Skeleton
+    ":skull:" : "skull.gif"
+
+
+};
